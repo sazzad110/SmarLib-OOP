44d95ee83d05aa2179abe49e0c34fee6b2b38851
sazzad <sazzadhasan610@gmail.com>
2026-05-19 17:36:54 +0600

    instance of book class/ objects of bokk

--- src/main/java/org/example/app/Main.java
+++ src/main/java/org/example/app/Main.java
@@ -1,7 +1,20 @@
 package org.example.app;
 
+import org.example.domain.Book;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to SmartLib Core");
+
+        Book book1 = new Book("Clean Code", "Robert C. Martin", "9780132350884", 2008);
+        Book book2 = new Book("Effective Java", "Joshua Bloch", "9780134685991", 2018);
+        Book book3 = new Book("Head First Java", "Kathy Sierra", "9780596009205", 2005);
+
+
+        book1.displayInfo();
+        book2.displayInfo();
+        book3.displayInfo();
+
+
     }
 }
